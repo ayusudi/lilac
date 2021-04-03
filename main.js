877588acef995862ffee0909f46589d1112f6a18
Ayu Sudi <ayusudi.abc@gmail.com>
2021-04-03 09:11:22 +0700

    create button click-press

--- main.js
+++ main.js
@@ -12,10 +12,25 @@ Vue.component('input-component', {
     'size'
   ],
   template: `
-    <div class="my-2 border-2 border-solid border-indigo-200 inline-block rounded bg-purple-100 focus-within:border-indigo-400">
+  <div>
+    <div class="my-2 border-2 border-solid border-indigo-200 inline-block rounded bg-purple-100 focus-within:border-indigo-300">
       <label for="email" class="px-1 text-gray-600" style="font-size: 11.5pt;">{{label}} : </label><input :type="type" :name="labelLowercase" :id="labelLowercase" class="rounded-r pl-2 input:bg-white " :size="size">
-    </div>`
+    </div>
+  </div>`
 })
+
+Vue.component('button-gradient', {
+  data  : function() {
+    return {
+
+    }
+  },
+  props : [],
+  template : `
+  
+  `
+})
+
 const app = new Vue({
   el: '#app',
   data: {
